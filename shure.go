b82c699c15c8b97634478c81df65d4345b933f4a
Joe <joseph.as.peterson@gmail.com>
2020-05-18 14:07:39 -0600

    basic structure

--- shure.go
+++ shure.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {}
+import "github.com/byuoitav/shure-audio-library/shure"
 
+func readEvents() error {
 
+	// go through all shure devices in room
+	// for each one start a go routine that waits for events to come in from that device
+	// push events to event hub
+
+	return nil
+}
+
+func waitForEvents(conn *shure.Connection) error {
+
+}
